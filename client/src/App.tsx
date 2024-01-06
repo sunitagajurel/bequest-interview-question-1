ba3d4f5e2b973da2d4c2bc9e5a21e8bed1159add
sunitagajurel <sunita.gajurel49@gmail.com>
2024-01-05 17:35:48 -0800

    verification and recovery completed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ const API_URL = "http://localhost:8080";
 
 function App() {
   const [data, setData] = useState<string>();
+  const [message, setMessage] = useState<string>();
+  const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
     getData();
@@ -29,7 +31,38 @@ function App() {
   };
 
   const verifyData = async () => {
-    throw new Error("Not implemented");
+    try {
+      const res = await fetch(`${API_URL}/verify`, {
+        method: "POST",
+        body: JSON.stringify({ data }),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.status === 200){
+          setMessage("your  data is safe , it was not tampered ")
+      }
+      else{
+        setMessage("OOPs! your data seems to be breached")
+        setError(true)
+      }
+    }
+    catch (err){
+      console.log(err)
+    }
+  };
+
+  const recoverData = async () => {
+    const res = await fetch(`${API_URL}/recover`)
+    const data = await res.json()
+    setData(data);
+    setError(false);
+    setMessage("your data has been successfully recoverd")
+  };
+
+  const textStyle = {
+    color: error ?'red' : 'green',
   };
 
   return (
@@ -63,6 +96,8 @@ function App() {
           Verify Data
         </button>
       </div>
+      <p style= {textStyle}> {message}</p>
+      { error? (<button onClick = {recoverData}> RecoverData </button>) : ""}
     </div>
   );
 }
