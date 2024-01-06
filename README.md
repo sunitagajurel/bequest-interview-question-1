31fd877a324b6dec772023d0f8d9f9b3140d1ea0
sunitagajurel <sunita.gajurel49@gmail.com>
2024-01-05 18:11:56 -0800

    updated on readme

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ Only the user is able to update their own data.
 
 
 **1. How does the client insure that their data has not been tampered with? Assume that the database is compromised.**
+A trustable copy of the data is always maintained/updated along with the encrypted value of the data which is separate from the database ( it can be stored somewhere in cloud or private data centre). To check whether the data is compromised or not we can check with the data on our private storage and We can compare the value of sent data  with both  our stored value and encrypted value to ensure the integrity of the data ' 
 <br />
 **2. If the data has been tampered with, how can the client recover the lost data?**
+The data from our private or trusted storage can send to the user back.
 
 
 Edit this repo to answer these two questions using any technologies you'd like, there any many possible solutions. Feel free to add comments.
@@ -18,3 +20,14 @@ Edit this repo to answer these two questions using any technologies you'd like, 
 1. Clone the repo
 2. Make a PR with your changes in your repo
 3. Email your github repository to robert@bequest.finance
+
+##Results 
+
+Tampered
+![alt text](./client/public/tampered.png?raw=true)
+
+Verified
+![alt text](./client/public/verified.png?raw=true)
+
+Recovered
+![alt text](./client/public/recovered.png?raw=true)
--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,28 @@
 import express from "express";
 import cors from "cors";
 
+const bcrypt = require("bcrypt")
+
 const PORT = 8080;
 const app = express();
 const database = { data: "Hello World" };
+const saltRounds = 10
+let salt:string
 
 app.use(cors());
 app.use(express.json());
+
+
+const verifiedData = {data:"",hash:""}
+
+const  generateSalt = async() => {
+  salt = await bcrypt.genSalt(saltRounds)
+}
+
+const hashData = async (data: string) => {
+  await generateSalt();
+  verifiedData.hash = await bcrypt.hash(data,salt)
+}
 
 // Routes
 
@@ -16,8 +32,39 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   database.data = req.body.data;
+  verifiedData.data = req.body.data;
+  hashData(req.body.data)
   res.sendStatus(200);
 });
+
+
+const verify = async(data: string) => {
+    const client_hash = await bcrypt.hash(data,salt)
+    if (client_hash === verifiedData.hash){
+      verifiedData.data = data;
+      return true
+    }
+   else{
+    return false
+   }
+}
+
+app.post("/verify", (req, res) => { 
+    verify(req.body.data).then((ans=> {
+      if (ans){
+        res.sendStatus(200)
+      }
+      else{
+        res.sendStatus(203)
+      }
+     
+    }))
+})
+
+//sending the last trusted data 
+app.get("/recover", (req, res) => { 
+  res.json(verifiedData.data)
+})
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
